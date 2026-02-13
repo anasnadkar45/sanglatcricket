1950bd823811cf164fd4bae15c8f0c0851d62511
anasnadkar45 <anasnadkar23@gmail.com>
2026-02-13 12:26:43 +0530

    feat: add login page with LoginCard component

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginCard from '@/components/login/LoginCard'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <LoginCard />
+    </div>
+  )
+}
+
+export default page
